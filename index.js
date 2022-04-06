8a6605d4817d70daded2af03579adcbb1dd3301a
Alina <pisarenko.alina@gmail.com>
2022-04-06 09:13:21 -0400

    commit tree b0780a56bf7b2e05ad2dc4ddc386304ee405d6e8

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat(name) {
+  const catsCopy = cats.slice();
+  catsCopy.push("Broom");
+  return catsCopy;
+}
+function prependCat(name) {
+  const catsCopy = cats.slice();
+  catsCopy.unshift("Arnold");
+  return catsCopy;
+}
+function removeLastCat(name) {
+  const catsCopy = cats.slice();
+  catsCopy.pop();
+  return catsCopy;
+}
+function removeFirstCat(name) {
+  const catsCopy = cats.slice();
+  catsCopy.shift();
+  return catsCopy;
+}
